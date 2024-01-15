040c2466d93001adbb3327ccde4811c982ba6827
WinsaDaniel <winsadaniel7@gmail.com>
2024-01-15 19:03:29 +0300

    fast- pizza-react

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import createOrder from "./features/order/createOrder";
+import CreateOrder from "./features/order/createOrder";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+    ],
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
