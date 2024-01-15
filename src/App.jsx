f2d496c27f187b6760fcc79044e85999fde763c4
WinsaDaniel <winsadaniel7@gmail.com>
2024-01-15 19:56:02 +0300

    fetching order data

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
-import createOrder from "./features/order/createOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+
 import CreateOrder from "./features/order/createOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
@@ -29,6 +29,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
       {
         path: "/order/new",
